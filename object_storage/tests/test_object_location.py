ef89385ea1e36c8fb2e77a05f8dc08e2fb10cbe8
mitchellgritts <mitchell@vibrantplanet.net>
2025-08-13 13:22:36 -0700

    working tests

--- object_storage/tests/test_object_location.py
+++ object_storage/tests/test_object_location.py
@@ -3,18 +3,18 @@ from object_storage.object_location import S3ObjectLocation
 
 def test_object_location():
     s3_object_location = S3ObjectLocation(
-        bucket_name="test-bucket",
-        key="test-key",
+        bucket="test-bucket",
+        path="test-key",
     )
-    assert s3_object_location.bucket_name == "test-bucket"
-    assert s3_object_location.key == "test-key"
+    assert s3_object_location.bucket == "test-bucket"
+    assert s3_object_location.path == "test-key"
     assert not s3_object_location.is_directory
     assert s3_object_location.s3_uri == "s3://test-bucket/test-key"
 
 
 def test_object_location_is_directory():
     s3_object_location = S3ObjectLocation(
-        bucket_name="test-bucket",
-        key="test-key/",
+        bucket="test-bucket",
+        path="test-key/",
     )
     assert s3_object_location.is_directory
