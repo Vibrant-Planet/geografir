fa6096b5475efd011db7dfbbe51d7b9c4b887452
mitchellgritts <mitchell@vibrantplanet.net>
2025-08-13 13:42:12 -0700

    tests for S3ObjectLocation

--- object_storage/tests/test_object_location.py
+++ object_storage/tests/test_object_location.py
@@ -18,3 +18,103 @@ def test_object_location_is_directory():
         path="test-key/",
     )
     assert s3_object_location.is_directory
+
+
+def test_object_location_serialize():
+    s3_object_location = S3ObjectLocation(
+        bucket="test-bucket",
+        path="test-key.tif",
+    )
+    s3_dir_location = S3ObjectLocation(
+        bucket="test-bucket",
+        path="test-key/",
+    )
+    assert s3_object_location.serialized == {
+        "bucket": "test-bucket",
+        "path": "test-key.tif",
+    }
+    assert s3_dir_location.serialized == {
+        "bucket": "test-bucket",
+        "path": "test-key/",
+    }
+
+
+def test_object_location_s3_uri():
+    s3_object_location = S3ObjectLocation(
+        bucket="test-bucket",
+        path="test-key.tif",
+    )
+    assert s3_object_location.s3_uri == "s3://test-bucket/test-key.tif"
+
+    s3_dir_location = S3ObjectLocation(
+        bucket="test-bucket",
+        path="test-key/",
+    )
+    assert s3_dir_location.s3_uri == "s3://test-bucket/test-key/"
+
+
+def test_object_location_extend():
+    s3_object_location = S3ObjectLocation(
+        bucket="test-bucket",
+        path="test-key/",
+    )
+    assert s3_object_location.extend("test-key.tif") == S3ObjectLocation(
+        bucket="test-bucket",
+        path="test-key/test-key.tif",
+    )
+
+
+def test_object_location_from_s3_uri():
+    s3_uri = "s3://test-bucket/test-key.tif"
+    assert S3ObjectLocation.from_s3_uri(s3_uri) == S3ObjectLocation(
+        bucket="test-bucket",
+        path="test-key.tif",
+    )
+
+    s3_uri = "s3://test-bucket/test-key/"
+    assert S3ObjectLocation.from_s3_uri(s3_uri) == S3ObjectLocation(
+        bucket="test-bucket",
+        path="test-key/",
+    )
+
+    s3_uri = "s3://test-bucket/test-key/test-key.tif"
+    assert S3ObjectLocation.from_s3_uri(s3_uri) == S3ObjectLocation(
+        bucket="test-bucket",
+        path="test-key/test-key.tif",
+    )
+
+
+def test_file_location_eq():
+    dummy_file_location = S3ObjectLocation(bucket="test-bucket", path="test/dummy.txt")
+    dummy_file_location_one = dummy_file_location.model_copy()
+    dummy_file_location_two = S3ObjectLocation(
+        bucket="test-bucket", path="test/works2.txt"
+    )
+    dummy_file_location_three = S3ObjectLocation(
+        bucket="test-bucket", path="test/works.txt"
+    )
+    dummy_file_location_four = S3ObjectLocation(
+        bucket="test-bucket", path="test/works2.txt"
+    )
+    assert dummy_file_location_one == dummy_file_location
+    assert dummy_file_location_two != dummy_file_location
+    assert dummy_file_location_three != dummy_file_location
+    assert dummy_file_location_four != dummy_file_location
+
+
+def test_file_location_hash():
+    dummy_file_location = S3ObjectLocation(bucket="test-bucket", path="test/dummy.txt")
+    dummy_file_location_one = dummy_file_location.model_copy()
+    dummy_file_location_two = S3ObjectLocation(
+        bucket="test-bucket", path="test/works2.txt"
+    )
+    dummy_file_location_three = S3ObjectLocation(
+        bucket="test-bucket", path="test/works.txt"
+    )
+    dummy_file_location_four = S3ObjectLocation(
+        bucket="test-bucket", path="test/works2.txt"
+    )
+    assert hash(dummy_file_location_one) == hash(dummy_file_location)
+    assert hash(dummy_file_location_two) != hash(dummy_file_location)
+    assert hash(dummy_file_location_three) != hash(dummy_file_location)
+    assert hash(dummy_file_location_four) != hash(dummy_file_location)
