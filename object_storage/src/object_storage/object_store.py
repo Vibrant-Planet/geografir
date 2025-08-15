4cdfdb2254cfb3958a5ccfee4247aab4f08e2ccb
Janet Wiener <janet@vibrantplanet.net>
2025-08-15 14:38:17 -0700

    ObjectStore (S3 read/write) part 2 [GEN-19813] (#11)

--- object_storage/src/object_storage/object_store.py
+++ object_storage/src/object_storage/object_store.py
@@ -3,16 +3,12 @@ from __future__ import annotations
 import logging
 import os
 
-import boto3
 from botocore.client import BaseClient
 
 from object_storage.object_location import ObjectLocation
 
 
-TEN_MINUTES = 600
-
-
-class ObjectStoreClient:
+class ObjectStore:
     """Wrapper class for client and other info that is common to all object store calls.
 
     Args:
@@ -27,26 +23,6 @@ class ObjectStoreClient:
     def __init__(self, s3_client: BaseClient, requester_pays: bool = False):
         self._s3_client = s3_client
         self._requester_pays = requester_pays
-
-    @staticmethod
-    def get_client(
-        aws_credentials: dict, region: str, requester_pays: bool = False
-    ) -> ObjectStoreClient:
-        """Constructs a boto3 s3 client function, to be used by various s3 helpers.
-
-        Can be replaced with a different function when mocking s3 buckets.
-
-        Args:
-            aws_credentials (dict): dict of aws_access_key_id and aws_secret_access_key to use
-            region (str): AWS region name
-            requester_pays (bool): whether a "requester pays" instruction should be injected into calls
-        """
-        return ObjectStoreClient(
-            s3_client=boto3.client(
-                "s3", region_name=region, aws_credentials=aws_credentials
-            ),
-            requester_pays=requester_pays,
-        )
 
     def list_files(
         self,
@@ -98,7 +74,7 @@ class ObjectStoreClient:
         """Download a single file from a S3 bucket.
 
         Args:
-            location (ObjectLocation): Object containing s3 bucket and full s3 file path
+            object_location (ObjectLocation): Object containing s3 bucket and full s3 file path
             local_directory (str): a destination directory for the file
             local_filename(str): an optional destination filename for the file. If not provided, uses the location path's basename
 
@@ -120,3 +96,62 @@ class ObjectStoreClient:
             f"Downloaded {object_location.bucket}:{object_location.path} to {download_path}"
         )
         return download_path
+
+    def download_directory(
+        self,
+        object_location: ObjectLocation,
+        local_directory: str,
+    ) -> list[str]:
+        """Download all files from a S3 bucket that begin with a given key prefix.
+
+        (i.e. a "folder" full of files that may vary.)
+
+        Args:
+            object_location (ObjectLocation): s3 bucket and full s3 file path
+            local_directory (str): a destination directory for the files
+
+        Returns:
+            List of all files that were downloaded.
+
+        Raises:
+            If any of files fails to download.
+                There is no attempt at cleanup of files that were downloaded prior to the failure.
+                If the caller needs cleanup, it should pass in a TemporaryDirectory.
+        """
+
+        remote_locations = self.list_files(object_location=object_location)
+
+        local_file_paths = [
+            self.download_file(
+                object_location=object_location,
+                local_directory=local_directory,
+            )
+            for object_location in remote_locations
+        ]
+        logging.debug(f"Downloaded {len(local_file_paths)} files in {local_directory}")
+        return local_file_paths
+
+    def upload_file(
+        self,
+        object_location: ObjectLocation,
+        local_filepath: str,
+    ) -> None:
+        """Uploads file to s3.
+
+        Args:
+            object_location (ObjectLocation): destination s3 bucket and full s3 file path
+            local_filename (str): source file to upload
+
+        Raises:
+            ValueError if local_filename does not exist
+        """
+        if not os.path.exists(local_filepath):
+            msg = f"File not found: {local_filepath}."
+            raise ValueError(msg)
+
+        self._s3_client.upload_file(
+            Filename=local_filepath,
+            Bucket=object_location.bucket,
+            Key=object_location.path,
+        )
+        logging.debug(f"Uploaded {local_filepath} to {object_location}")
---
+++ object_storage/tests/test_object_store.py
@@ -0,0 +1,128 @@
+import os
+import pytest
+import boto3
+from moto import mock_aws
+from tempfile import TemporaryDirectory
+
+from object_storage.object_store import ObjectStore
+from object_storage.object_location import ObjectLocation
+
+TEST_BUCKET = "test_bucket"
+
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
+
+
+@pytest.fixture(scope="function")
+def s3(aws_credentials):
+    """
+    Return a mocked S3 client
+    """
+    with mock_aws():
+        yield boto3.client("s3", region_name="us-east-1")
+
+
+def create_files(s3):
+    s3.create_bucket(Bucket=TEST_BUCKET)
+    s3.put_object(Bucket=TEST_BUCKET, Key="path/one.txt", Body="test1")
+    s3.put_object(Bucket=TEST_BUCKET, Key="path/two.txt", Body="test2")
+
+
+def test_list_files(s3):
+    create_files(s3)
+    client = ObjectStore(s3_client=s3)
+
+    object_location = ObjectLocation(bucket=TEST_BUCKET, path="path/")
+    files = client.list_files(object_location)
+
+    assert files == [
+        ObjectLocation(bucket=TEST_BUCKET, path="path/one.txt"),
+        ObjectLocation(bucket=TEST_BUCKET, path="path/two.txt"),
+    ]
+
+
+def test_download_file(s3):
+    create_files(s3)
+    client = ObjectStore(s3_client=s3)
+
+    object_location = ObjectLocation(bucket=TEST_BUCKET, path="path/one.txt")
+
+    with TemporaryDirectory() as tmpdir:
+        local_filename = client.download_file(
+            object_location=object_location,
+            local_directory=tmpdir,
+        )
+        assert local_filename == os.path.join(tmpdir, "one.txt")
+        assert os.path.exists(local_filename)
+        with open(local_filename) as f:
+            assert f.read() == "test1"
+
+
+def test_download_file_with_local_name_override(s3):
+    create_files(s3)
+    client = ObjectStore(s3_client=s3)
+
+    object_location = ObjectLocation(bucket=TEST_BUCKET, path="path/one.txt")
+
+    with TemporaryDirectory() as tmpdir:
+        local_filename = client.download_file(
+            object_location=object_location,
+            local_directory=tmpdir,
+            local_filename="override.txt",
+        )
+        assert local_filename == os.path.join(tmpdir, "override.txt")
+        assert os.path.exists(local_filename)
+        with open(local_filename) as f:
+            assert f.read() == "test1"
+
+
+def test_download_directory(s3):
+    create_files(s3)
+    client = ObjectStore(s3_client=s3)
+
+    object_location = ObjectLocation(bucket=TEST_BUCKET, path="path/")
+
+    with TemporaryDirectory() as tmpdir:
+        local_filenames = client.download_directory(
+            object_location=object_location,
+            local_directory=tmpdir,
+        )
+        assert len(local_filenames) == 2
+
+        assert os.path.exists(os.path.join(tmpdir, "one.txt"))
+        assert os.path.exists(os.path.join(tmpdir, "two.txt"))
+
+
+def test_upload_file(s3):
+    create_files(s3)
+    client = ObjectStore(s3_client=s3)
+
+    upload_location = ObjectLocation(bucket=TEST_BUCKET, path="path/")
+    object_location = upload_location.extend("upload.txt")
+
+    with TemporaryDirectory() as tmpdir:
+        filepath = os.path.join(tmpdir, "upload.txt")
+        with open(filepath, "w") as f:
+            f.write("upload me!")
+
+        client.upload_file(
+            object_location=object_location,
+            local_filepath=filepath,
+        )
+
+    with TemporaryDirectory() as tmpdir:
+        local_filename = client.download_file(
+            object_location=object_location,
+            local_directory=tmpdir,
+        )
+        assert local_filename == os.path.join(tmpdir, "upload.txt")
+        assert os.path.exists(local_filename)
+        with open(local_filename) as f:
+            assert f.read() == "upload me!"
